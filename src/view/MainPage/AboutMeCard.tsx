e9904874e1e7e0cc7394bb095958ceb46f663d16
Joel Campos <joelcamposoliva9000@gmail.com>
2022-06-06 16:25:00 +0200

    Basic links (Installed icons)

--- src/view/MainPage/AboutMeCard.tsx
+++ src/view/MainPage/AboutMeCard.tsx
@@ -1,5 +1,27 @@
-import { Card, CardContent, Grid } from '@mui/material';
+import { Card, CardContent, Grid, IconButton } from '@mui/material';
 import FitImage from '../../components/images/FitImage';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
+type Social = {
+    icon: JSX.Element;
+    link: string;
+}
+
+const social: Social[] = [
+    {
+        icon: <LinkedInIcon/>,
+        link: 'https://www.linkedin.com/in/joelcamposoliva/',
+    },
+    {
+        icon: <GitHubIcon/>,
+        link: 'https://github.com/TheMineWay',
+    }
+];
+
+const navigate = (url: string) => {
+    window.open(url, '_blank');
+}
 
 export default function AboutMeCard() {
     return (
@@ -35,9 +57,32 @@ export default function AboutMeCard() {
                         md={8}
                         style={{
                             paddingLeft: '2em',
+                            paddingBottom: '2em',
+                            display: 'flex',
+                            alignContent: 'space-between',
+                            flexDirection: 'column',
+                            alignItems: 'stretch',
                         }}
                     >
-                        <h1>Joel Campos Oliva</h1>
+                        <div
+                            style={{
+                                height: '100%',
+                            }}
+                        >
+                            <h1>Joel Campos Oliva</h1>
+                        </div>
+                        <div>
+                            {
+                                social.map((s) => (
+                                    <IconButton
+                                        color='primary'
+                                        onClick={() => navigate(s.link)}
+                                    >
+                                        {s.icon}
+                                    </IconButton>
+                                ))
+                            }
+                        </div>
                     </Grid>
                 </Grid>
             </CardContent>
