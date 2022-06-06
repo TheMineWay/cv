fdef6fa8263eae23ee61a8e80e9feac6c3a90cb5
Joel Campos <joelcamposoliva9000@gmail.com>
2022-06-06 21:34:03 +0200

    Main page grid

---
+++ src/view/MainPage/AcademicVoyage/AcademicVoyage.tsx
@@ -0,0 +1,7 @@
+export default function AcademicVoyage() {
+    return (
+        <>
+            
+        </>
+    );
+}
--- src/view/MainPage/MainPage.tsx
+++ src/view/MainPage/MainPage.tsx
@@ -1,9 +1,29 @@
+import { Grid } from "@mui/material";
 import AboutMe from "./AboutMe/AboutMe";
+import AcademicVoyage from "./AcademicVoyage/AcademicVoyage";
 
 export default function MainPage() {
+
+    const parts: JSX.Element[] = [
+        <AboutMe />,
+        <AcademicVoyage />,
+    ];
+
     return (
-        <>
-            <AboutMe/>
-        </>
+        <Grid
+            container
+            spacing={4}
+        >
+            {
+                parts.map((part) => (
+                    <Grid
+                        item
+                        xs={12}
+                    >
+                        {part}
+                    </Grid>
+                ))
+            }
+        </Grid>
     );
 }
