061c0d43d8f87d6b895c571f81c21ffbda19ae05
Joel Campos <joelcamposoliva9000@gmail.com>
2022-06-06 21:38:37 +0200

    Base voyage card

--- src/view/MainPage/AcademicVoyage/AcademicVoyage.tsx
+++ src/view/MainPage/AcademicVoyage/AcademicVoyage.tsx
@@ -1,7 +1,11 @@
+import { Card, CardContent } from "@mui/material";
+
 export default function AcademicVoyage() {
     return (
-        <>
-            
-        </>
+        <Card>
+            <CardContent>
+                
+            </CardContent>
+        </Card>
     );
 }
